a4aed9cca3abe798b90dd146fd458a6c6f1138f4
Evan Lee <evan.lee7007@gmail.com>
2022-07-26 16:04:58 +1000

    moved tensors to cuda

--- crowd_nav/utils/transform.py
+++ crowd_nav/utils/transform.py
@@ -10,6 +10,7 @@ class MultiAgentTransform():
     def transform_frame(self, frame):
         bs = frame.shape[0]
         compare = frame.unsqueeze(1) - frame.unsqueeze(2)                  # [length, num_human, num_human, state_dim]
-        relative = torch.masked_select(compare, self.mask.repeat(bs, 1, 1, 1)).reshape(bs, self.num_human, -1)   # [length, num_human, (num_human-1) * state_dim]
+        compare.to(device='cuda')
+        relative = torch.masked_select(compare, self.mask.repeat(bs, 1, 1, 1).to(device='cuda')).reshape(bs, self.num_human, -1)   # [length, num_human, (num_human-1) * state_dim]
         state = torch.cat([frame, relative], axis=2)
         return state
